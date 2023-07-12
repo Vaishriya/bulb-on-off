82972374f6ade68d04e475382541a558aa0c5c80
Vaishriya <139196955+Vaishriya@users.noreply.github.com>
2023-07-12 13:57:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var on_bt=document.getElementById("on");
+var off_bt=document.getElementById("off");
+var img=document.getElementById("bulb")
+function bulb_on()
+{
+    img.src="./image/kutty.jpg";
+}
+function bulb_off()
+{
+    img.src="./image/v.jpg";
+}
